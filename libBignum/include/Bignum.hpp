f379a461cfea6520a0a6a14c86503970a0cccc76
snocrash <1131909602@qq.com>
2022-04-22 20:58:49 +0800

    输入输出运算符重载

--- libBignum/include/Bignum.hpp
+++ libBignum/include/Bignum.hpp
@@ -4,15 +4,14 @@ class BigNum
 {
 public:
     BigNum() {}
-    BigNum(std::string num);
 
-    void setNum(std::string num);
-    void print();
-    void pop(char num);
-
+    friend std::istream &operator>>(std::istream &input, BigNum &bignum);
+    friend std::ostream &operator<<(std::ostream &output, BigNum &bignum);
     BigNum operator=(BigNum num);
     friend BigNum operator+(const BigNum& bignum_a, const BigNum& bignum_b);
 
 private:
+    void pop(char num);
+
     std::string m_num;
 };
